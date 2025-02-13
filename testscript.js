e50ab24214da174fd643df23efeed662adf8809b
Richard <richard.yao@antithesis.com>
2025-02-13 14:38:28 -0500

    Update: cleaned up dockerfile

--- testscript.js
+++ testscript.js
@@ -8,9 +8,9 @@ const privateKey = "0x12d7de8621a77640c9241b2595ba78ce443d05e94090365ab3bb5e19df
 const account = web3.eth.accounts.privateKeyToAccount(privateKey);
 const fromAddress = account.address;
 
-const toAddress = fromAddress; // Sending to yourself
+const toAddress = fromAddress; 
 const valueWei = web3.utils.toHex('10000000000000000'); // 0.01 Matic in Wei (hex)
-const gasPriceHex = web3.utils.toHex(0); // Gas price 0
+const gasPriceHex = web3.utils.toHex(0); 
 const gasLimitHex = web3.utils.toHex(21000);
 let nonceHex;
 let chainIdHex;
@@ -27,7 +27,7 @@ async function sendMatic() {
       to: toAddress,
       value: valueWei,
       data: '0x0', // No data for Matic transfer
-      chainId: chainIdHex, // Use the dynamic Chain ID
+      chainId: chainIdHex,
     };
 
     const signedTx = await account.signTransaction(txParams);
